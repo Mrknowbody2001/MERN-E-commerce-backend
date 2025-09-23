5e1fbfa93f1efa80d56a0bedd837f9ba6dbb1a14
W.A Chamith Sandeepa Thilakarathna <sandeepachamith53@gmail.com>
2025-09-23 21:22:48 +0530

    route protected

--- routes/OrderRoutes.js
+++ routes/OrderRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { confirmOrder, createOrder } from "../controllers/OrderController.js";
+import { IsAuth } from "../middleware/Auth.js";
 
 const orderRouter = express.Router();
 
-orderRouter.post("/create", createOrder);
-orderRouter.post("/confirm", confirmOrder);
+orderRouter.post("/create",IsAuth, createOrder);//protected route
+orderRouter.post("/confirm",IsAuth, confirmOrder);//protected route 
 
 export default orderRouter;
