44a6b9d33746caae7fe10571754284c6cd49883d
W.A Chamith Sandeepa Thilakarathna <sandeepachamith53@gmail.com>
2025-09-25 10:09:30 +0530

    ...

--- models/Product.js
+++ models/Product.js
@@ -6,7 +6,12 @@ const SizeSchema = new mongoose.Schema({
     enum: ["S", "M", "L", "XL", "28", "30", "32", "34"],
     required: true,
   },
-  quantity: { type: Number, required: true, min: 0 }, // 👈 add quantity per size
+  quantity: {
+    type: Number,
+
+    required: true,
+    min: 0,
+  }, // 👈 add quantity per size
 });
 
 const ProductSchema = new mongoose.Schema(
@@ -17,7 +22,7 @@ const ProductSchema = new mongoose.Schema(
     image: { type: String, required: true },
     category: {
       type: String,
-      enum: ["Men", "Women","unisex", "Kids"],
+      enum: ["Men", "Women", "unisex", "Kids"],
       required: true,
     },
     sizes: [SizeSchema], // 👈 now stores array of { size, quantity }
